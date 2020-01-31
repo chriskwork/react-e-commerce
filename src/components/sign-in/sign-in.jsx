9202273f61503832613b652e84f7a8294e25e402
chris k <yovirinc@gmail.com>
2020-01-31 03:56:35 +0100

    implented firebase auth code

--- src/components/sign-in/sign-in.jsx
+++ src/components/sign-in/sign-in.jsx
@@ -3,6 +3,8 @@ import FormInput from "../form-input/form-input";
 import CustomButton from "../custom-button/custom-button";
 import "./sign-in.scss";
 
+import { signInWithGoogle } from "../../firebase/firebase.utils";
+
 class SignIn extends React.Component {
   state = {
     email: "",
@@ -46,7 +48,12 @@ class SignIn extends React.Component {
             required
           />
 
-          <CustomButton type="submit">Sign in</CustomButton>
+          <div className="buttons">
+            <CustomButton type="submit">Sign in</CustomButton>
+            <CustomButton isGoogleSignIn onClick={signInWithGoogle}>
+              Sign in with Google
+            </CustomButton>
+          </div>
         </form>
       </div>
     );
---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,26 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const config = {
+  apiKey: "AIzaSyDkhAyMAq8QWrFpCeM6BXgc5zmZse-oTWI",
+  authDomain: "shop-tutorial-db.firebaseapp.com",
+  databaseURL: "https://shop-tutorial-db.firebaseio.com",
+  projectId: "shop-tutorial-db",
+  storageBucket: "shop-tutorial-db.appspot.com",
+  messagingSenderId: "601950552759",
+  appId: "1:601950552759:web:9e6279a96adde62efe8332",
+  measurementId: "G-HJLYQQF2Y9"
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: "select_account" });
+
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
